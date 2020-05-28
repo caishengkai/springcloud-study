cb6d85c7a42362c6a8a935f8f1dd1c7b21b3beec
caishengkai <312039449@qq.com>
2020-05-28 15:02:43 +0800

    服务注册与发现

---
+++ README.md
@@ -0,0 +1,1 @@
+系统学习SpringCloud,参考https://github.com/macrozheng/springcloud-learning
---
+++ eureka-security-server/src/main/java/com/csk/cloud/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.csk.cloud.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @description: springsecurity配置类
+ * @author: caishengkai
+ * @time: 2020/5/28 14:29
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
