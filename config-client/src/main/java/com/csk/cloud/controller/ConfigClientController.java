df65cb76850fc2e3b669fc4672174e68890b4d2b
caishengkai <312039449@qq.com>
2020-06-01 09:32:33 +0800

    config-client外部集中化配置client模块

---
+++ config-client/src/main/java/com/csk/cloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.csk.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: caishengkai
+ * @time: 2020/5/31 16:37
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+}
+}
