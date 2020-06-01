38ded4583e547c602de6d874353868dfe55b4cf0
caishengkai <312039449@qq.com>
2020-06-01 09:28:06 +0800

    Hystrix-dashboard熔断器监控模块

---
+++ hystrix-dashboard/src/test/java/com/csk/cloud/HystrixDashboardApplicationTests.java
@@ -0,0 +1,14 @@
+package com.csk.cloud;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HystrixDashboardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println(1);
+    }
+
+}
